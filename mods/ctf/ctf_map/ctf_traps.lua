21deb2de1d76e696ffff01893ed4b03f04bd1d56
pl608 <computerprojects46@gmail.com>
2022-12-07 13:57:04 -0800

    Update ctf_traps.lua

--- mods/ctf/ctf_map/ctf_traps.lua
+++ mods/ctf/ctf_map/ctf_traps.lua
@@ -132,60 +132,62 @@ minetest.register_entity("ctf_map:check_player", {
 			end
 		end]]
 		local obj = self.object
-		local pos = object:get_pos()
+		local pos = obj:get_pos()
 		local plyrs = minetest.get_objects_inside_radius(pos, 3)
 
-		minetest.add_particlespawner({
-			amount = 20,
-			time = 0.5,
-			minpos = vector.subtract(pos, 3),
-			maxpos = vector.add(pos, 3),
-			minvel = {x = 0, y = 5, z = 0},
-			maxvel = {x = 0, y = 7, z = 0},
-			minacc = {x = 0, y = 1, z = 0},
-			maxacc = {x = 0, y = 1, z = 0},
-			minexptime = 0.3,
-			maxexptime = 0.6,
-			minsize = 7,
-			maxsize = 10,
-			collisiondetection = true,
-			collision_removal = false,
-			vertical = false,
-			texture = "grenades_smoke.png",
-		})
-
-		minetest.add_particle({
-			pos = pos,
-			velocity = {x=0, y=0, z=0},
-			acceleration = {x=0, y=0, z=0},
-			expirationtime = 0.3,
-			size = 15,
-			collisiondetection = false,
-			collision_removal = false,
-			object_collision = false,
-			vertical = false,
-			texture = "grenades_boom.png",
-			glow = 10
-		})
-
-		minetest.sound_play("grenades_explode", {
-			pos = pos,
-			gain = 1.0,
-			max_hear_distance = 64,
-		})
+		
 
 		for _, v in pairs(plyrs) do
 			if v:is_player() and ctf_teams.get(v:get_player_name()) ~=  self._team then
+						minetest.add_particlespawner({
+					amount = 20,
+					time = 0.5,
+					minpos = vector.subtract(pos, 3),
+					maxpos = vector.add(pos, 3),
+					minvel = {x = 0, y = 5, z = 0},
+					maxvel = {x = 0, y = 7, z = 0},
+					minacc = {x = 0, y = 1, z = 0},
+					maxacc = {x = 0, y = 1, z = 0},
+					minexptime = 0.3,
+					maxexptime = 0.6,
+					minsize = 7,
+					maxsize = 10,
+					collisiondetection = true,
+					collision_removal = false,
+					vertical = false,
+					texture = "grenades_smoke.png",
+				})
+
+				minetest.add_particle({
+					pos = pos,
+					velocity = {x=0, y=0, z=0},
+					acceleration = {x=0, y=0, z=0},
+					expirationtime = 0.3,
+					size = 15,
+					collisiondetection = false,
+					collision_removal = false,
+					object_collision = false,
+					vertical = false,
+					texture = "grenades_boom.png",
+					glow = 10
+				})
+
+				minetest.sound_play("grenades_explode", {
+					pos = pos,
+					gain = 1.0,
+					max_hear_distance = 64,
+				})
 				if placerobj then
 					v:punch(placerobj, 1, {damage_groups = {fleshy = 15, landmine = 1}})
 				else
 					local chp = v:get_hp()
 					v:set_hp(chp - 15)
+					minetest.remove_node(pos)
+					obj:remove()
 				end
 			end
 		end
-		minetest.remove_node(pos)
-		end
+	end
 })
 minetest.register_node("ctf_map:landmine", {
 	description = "Landmine",
